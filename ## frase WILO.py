284adfa3fdee1199861cb51261fe0ab945e31577
WiOMG13 <105610654+WiOMG13@users.noreply.github.com>
2024-01-20 11:22:07 -0500

    Add files via upload

---
+++ ## frase WILO.py
@@ -0,0 +1,41 @@
+## frase WILO
+mi_frase = "SOY WILSON Y ME GUSTA LOS VIDEOJUEGOS"
+print(mi_frase)
+
+## numero de VIDEOJUEGOS INSTALADOS 
+nVIDE =[1,2,3,4,5]
+print(nVIDE)
+
+
+##LISTA
+
+VIDEOJUEGOS = {"1":"FIFA21", "2":"FIFA22", "3":"FORNITE"}
+print(VIDEOJUEGOS)
+
+
+############ DECLARAR UN VECTOR DE ENTEROS
+vector_enteros=[5]*70
+print(vector_enteros)
+
+vector_flotantes=[4.22]*6
+print(vector_flotantes)
+
+diccionario = {"entero" : vector_enteros, "flotante" : vector_flotantes}
+print(diccionario)
+
+
+#CADENAS
+#SALIDAS Y MENSAJES
+#CREACIÓN DE IDENTIFICADORES
+cadena_simple = 'Hola a todos!'
+
+#Comida favorita
+cadena_doble=["hamburguesa","encebollado"]
+print(cadena_doble)
+print(cadena_simple)
+
+
+# Tabla Excel usando pandas
+import pandas as pd
+imp_sri= pd.read_excel("ventas_SRI.xlsx")
+print(imp_sri)
